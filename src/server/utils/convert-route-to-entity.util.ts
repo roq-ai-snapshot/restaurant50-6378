4465bb539328685d1a1c0758d025530488011c36
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-06-25 10:51:04 +0000

    Initial commit

---
+++ src/server/utils/convert-route-to-entity.util.ts
@@ -0,0 +1,12 @@
+const mapping: Record<string, string> = {
+  'menu-items': 'menu_item',
+  orders: 'order',
+  'order-items': 'order_item',
+  restaurants: 'restaurant',
+  tables: 'table',
+  users: 'user',
+};
+
+export function convertRouteToEntityUtil(route: string) {
+  return mapping[route] || route;
+}
